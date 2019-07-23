691d562aeb8bdc7bca0a69643c8cb30acdb28d6c
Uchio Kondo <udzura@udzura.jp>
2019-07-24 01:11:19 +0900

    First implementation

--- ext/dtrace_probe_simple/dtrace_probe_simple.c
+++ ext/dtrace_probe_simple/dtrace_probe_simple.c
@@ -1,9 +1,21 @@
+#include <sys/sdt.h>
 #include "dtrace_probe_simple.h"
 
-VALUE rb_mDtraceProbeSimple;
+static VALUE
+rb_do_probe_simple (VALUE mod, VALUE arg0) {
+  if(FIXNUM_P(arg0)) {
+    DTRACE_PROBE1(ruby-probe, ruby, NUM2LONG(arg0));
+  } else {
+    DTRACE_PROBE1(ruby-probe, ruby, StringValuePtr(arg0));
+  }
+  return arg0;
+}
 
 void
 Init_dtrace_probe_simple(void)
 {
+  VALUE rb_mDtraceProbeSimple;
+
   rb_mDtraceProbeSimple = rb_define_module("DtraceProbeSimple");
+  rb_define_module_function(rb_mDtraceProbeSimple, "probe", rb_do_probe_simple, 1);
 }
--- lib/dtrace_probe_simple.rb
+++ lib/dtrace_probe_simple.rb
@@ -2,6 +2,4 @@ require "dtrace_probe_simple/version"
 require "dtrace_probe_simple/dtrace_probe_simple"
 
 module DtraceProbeSimple
-  class Error < StandardError; end
-  # Your code goes here...
 end
